df0b4f709de3ae0a904866cb9b17edf206fdcbc4
Erick Hernandez <yerickk8@gmail.com>
2022-12-13 12:18:44 -0600

    Implemented the crypto currency list, sorting function and search function

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-coin-list',
@@ -8,6 +11,11 @@ import { ApiService } from '../services/api.service';
 })
 export class CoinListComponent implements OnInit {
 
+  bannerData: any = [];
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['symbol', 'current_price', 'market_cap_change_percentage_24h', 'market_cap'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
@@ -18,15 +26,29 @@ export class CoinListComponent implements OnInit {
   getBannerData() {
     this.api.getTrendingCurrency("INR").subscribe(res => {
       console.log(res);
+      this.bannerData = res;
     })
 
   }
 
-  getAllData() { 
+  getAllData() {
     this.api.getCurrency("INR").subscribe(res => {
       console.log(res);
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
 
     })
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+
